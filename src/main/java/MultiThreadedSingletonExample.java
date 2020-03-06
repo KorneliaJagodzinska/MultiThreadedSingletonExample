f0743239f1fb27d0ea0c47361b227c673a59c9b3
Kornelia Jagodzińska <kornelia.jagodzinska@gmail.com>
2020-03-06 13:32:28 +0100

    Multithreaded Singleton Example

---
+++ src/main/java/MultiThreadedSingletonExample.java
@@ -0,0 +1,17 @@
+public class MultiThreadedSingletonExample {
+    private static MultiThreadedSingletonExample instance;
+
+    private MultiThreadedSingletonExample() {
+    }
+
+    public static MultiThreadedSingletonExample getInstance() {
+        if (instance == null) {
+            synchronized (MultiThreadedSingletonExample.class) {
+                if (instance == null) {
+                    instance = new MultiThreadedSingletonExample();
+                }
+            }
+        }
+        return instance;
+    }
+}
